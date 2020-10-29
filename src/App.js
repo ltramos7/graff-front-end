83b4d0e033b39056a48aa2652f16534fecc9ef36
Linda <ltramos7@gmail.com>
2020-10-29 14:43:52 -0600

    commented plans for next steps.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 import Signup from './components/signup'
-import SignupHooks from './components/SignupHooks'
+// import SignupHooks from './components/SignupHooks'
 import UserProfile from './components/UserProfile'
+
+// a hooks version of signup component might not be ideal. I have to lift the state to the app component.
+
+// make sure I can lift state to app
+// pass the state information to profile or figure out how to add redux at this point. 
 
 
 
 function App() {
   return (
     <div>
-      {/* <Signup/> */}
-      <SignupHooks/>
+      <Signup/>
+      {/* <SignupHooks/> */}
       <UserProfile/>
 
       
