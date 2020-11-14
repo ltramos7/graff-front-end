c707f83aeaa56dc37f48e1ef5c5d9c8857a60a33
Linda <ltramos7@gmail.com>
2020-11-14 14:31:21 -0600

    can grab user info to login, but need to check if user already exists to login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react'
 
 class Navigation extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            loggedIn: false
+        }
+    }
+
+    handleInputChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            [e.target.name]: e.target.value,
+            
+        })
+        
+    }
+
+    handleLogin = (e) => {
+        e.preventDefault()
+        console.log("username:", this.state.username)
+        console.log("password:", this.state.password)
+
+        //need to compare if the user signing in already exists. If so, they get redirected to their profile page. If not they get redirected to sign up page
+    }
+
+
     render(){
         return(
             <div className="navigation-container">
                 <h1>Graffito</h1>
                 <div className="buttons">
-                    <form>
-                        <input placeholder="User Name"/>
-                        <input placeholder="Password"/>
+                    <form onSubmit={this.handleLogin}>
+                        <input placeholder="User Name" name="username" value={this.state.value}onChange={this.handleInputChange}/>
+                        <input placeholder="Password" name="password" value={this.state.value} onChange={this.handleInputChange}/>
                         <input type="submit" value="Log In"/>
                     </form>
 
